afe79f3cdb4635b974cc1d0c2cf3a5f1481441b5
thomas rafael czank <thomas.czank@gmail.com>
2019-06-27 16:29:45 +0900

    igal's corrections seems to have worked, still BaBar is better

--- nobs_nexp_v0.C
+++ nobs_nexp_v0.C
@@ -54,7 +54,7 @@ void nobs_nexp_v0() {
   // ###############################################################//
 
   //Output File //
-  TFile * f = new TFile("nexp_nobs_merge.root","RECREATE");
+  TFile * f = new TFile("cor_nexp_nobs_merge.root","RECREATE");
   TH2D * nexp = new TH2D("h_nexp_gp_m", "number of expected events by Z' coupling strength and mass;m_{Z'}[GeV/c^{2}];g';number of expected events;", 10000,0.0,10.0,10000,-5.,1.0);
   TH1F * nexp_x = new TH1F("h_nexp_gp_m_x", "number of expected events by Z' mass Xproject;m_{Z'}[GeV/c^{2}];number of expected events;", 10000,0.0,10.0);
   TH1F * nexp_y = new TH1F("h_nexp_gp_m_y", "number of expected events by Z' coupling strength Yproject;g';number of expected events;", 10000,-5.,1.);
@@ -90,6 +90,7 @@ void nobs_nexp_v0() {
   for(j;j<10000;j++){
     //while( mass < 9.21){
     for(int i = 0;i<10000;i++){
+      if(nexp_x->GetBinCenter(i+1) >= 0.212){
       double_t mass = nexp_x->GetBinCenter(i+1);
       double_t gz = nexp_y->GetBinCenter(j+1);
     //  cout << " the value of i and j is " << i << " " << j << endl;
@@ -101,7 +102,7 @@ void nobs_nexp_v0() {
       }
       double brlumdet = gr_mu->Eval(mass) * deteff_fit->Eval(mass) * ((up1sxs->Eval(mass)*1e-3*4.77836) + (up2sxs->Eval(mass)*1e-3*3.5135) + (up3sxs->Eval(mass)*1e-3*16.89427) + (up4sxs->Eval(mass)*1e-3*690.555) + (up5sxs->Eval(mass)*1e-3*123.81655) + continuum_th_lum_all );
       //       cout << " the deteff mubr and brlumdet are " << deteff_fit->Eval(mass) << " " << gr_mu->Eval(mass) << " " << brlumdet << endl;
-      nobs->GetPoint(k,vXout,vYout);
+      nobs->GetPoint(i-212-1,vXout,vYout);
       double nexp_n = pow(gz,2)*(brlumdet/pow(0.1,2));
       double gp_val = nexp_n/vYout;
       if(gp_val > 1.){
@@ -111,6 +112,7 @@ void nobs_nexp_v0() {
       nexp->SetBinContent(i+1,j+1,nexp_n);
       continuum_th_lum = 0;
       continuum_th_lum_all = 0;
+      }
     }
     }
 
